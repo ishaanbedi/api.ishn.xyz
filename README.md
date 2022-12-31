8bd2b624e850ab2aa820031a6e4669bb3fdad6ef
Ishaan Bedi <bediishaan6@gmail.com>
2022-12-31 21:52:21 +0530

    INIT | api.ishn.xyz

---
+++ README.md
@@ -0,0 +1,99 @@
+<head>
+</head>
+
+# api.ishn.xyz
+
+This API allows users to create short URLs for long URLs, built on top of [ishn.xyz](https://www.ishn.xyz), a fast and minimal link shortener.
+
+## Link
+
+The api is accessible at [api.ishn.xyz](https://api.ishn.xyz)
+
+## Endpoints
+
+Create a short URL:
+**_GET /v1?url=<long_url>_**
+
+This endpoint creates a short URL for the specified long URL.
+
+## Parameters
+
+- url: The long URL to be shortened. (required)
+
+## Response
+
+On success, the API returns a JSON object with the following properties:
+
+- success: A boolean indicating whether the request was successful (true in this case)
+- slug: The short URL slug, without ishn.xyz.
+- url: The complete shortened URL.
+- original: The original long URL.
+- status_code: The HTTP status code of the response.
+
+On error, the API returns a JSON object with the following properties:
+error: An error message.
+
+- success: A boolean indicating whether the request was successful. (false in this case)
+- status_code: The HTTP status code of the response.
+- hint: A hint on how to fix the error.
+
+## API Limits
+
+To ensure fair usage and to protect our resources, this API has the following limits in place:
+
+- **Rate limits**: 10 requests per minute
+
+Please be mindful of the limits when using our API.
+
+If you exceed a limit, you may receive an error message or your requests may be blocked until the limit resets.
+
+If you need to exceed the API rate limits for a genuine use case, please contact me at [hi@ishaanbedi.in](mailto:hi@ishaanbedi.in) with a brief description of your use case. I might be able to lift the rate limits for you if your use-case does not negatively impact the resources.
+
+Thank you for respecting the API rate limits
+
+## Using the API
+
+There are several ways to use the API, including using a terminal, a browser, or a tool like Postman, Insomnia or Thunder.
+
+### Using in a Terminal
+
+You can use the API using a terminal by using `curl` or `wget`.
+
+#### Example with curl
+
+To test the `GET /v1` endpoint, use the following command:
+
+```
+curl https://api.ishn.xyz/v1?url=https://www.github.com
+```
+
+This will send a request to the API and return the response.
+
+#### Example with wget
+
+To test the `GET /v1` endpoint, use the following command:
+
+```
+wget -qO- https://api.ishn.xyz/v1?url=https://www.github.com
+```
+
+This will send a request to the API and print the response to the terminal.
+
+### By using in a Browser
+
+You can also use/test the API using a browser by simply visiting the API endpoint URL in the address bar.
+
+For example, to test the `GET /v1` endpoint, visit the following URL in your browser:
+
+```
+https://api.ishn.xyz/v1?url=https://www.github.com
+```
+
+This will send a request to the API and display the response in the browser.
+
+
+** To get more info about this API and understand the complete architecture, please refer to [api.ishn.xyz/info](https://api.ishn.xyz/info).
+
+## License
+
+This API is licensed under the MIT License. See the LICENSE file for more information.
---
+++ app.js
@@ -0,0 +1,170 @@
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const XataClient = require("./src/xata").XataClient;
+var parse = require("url-parse");
+require("dotenv").config();
+const xata = new XataClient({ apiKey: process.env.XATA_API_KEY });
+const app = express();
+const limiter = rateLimit({
+  windowMs: 10 * 1000,
+  max: 10,
+  handler: (req, res) => {
+    res.status(429).json({
+      error: "Too many requests.",
+      reason: `You have exceeded the request limit. For security reasons, you can only make 10 requests per minute.`,
+      success: false,
+      status_code: 429,
+      hint: "Please wait for a few moments before making another request.",
+    });
+  },
+  keyGenerator: (req) => {
+    return req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  },
+});
+app.use((req, res, next) => {
+  if (req.path === "/" || req.path === "/v1/info") {
+    next();
+  } else {
+    limiter(req, res, next);
+  }
+});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/v1/info", async (req, res) => {
+  res.status(200).json({
+    success: true,
+    status_code: 200,
+    info: {
+      name: "api.ishn.xyz",
+      description: "A URL shortener API built on top of ishn.xyz",
+      version: "1.0.0",
+      v1: {
+        description: "The first version of the API.",
+        endpoints: {
+          "/v1": {
+            description: "Creates a new URL shortener.",
+            method: "GET",
+            query_parameters: {
+              url: {
+                description: "The URL to shorten.",
+                type: "string",
+                required: true,
+              },
+            },
+            response: {
+              success: {
+                description: "Whether the request was successful or not.",
+                type: "boolean",
+              },
+              slug: {
+                description: "The slug of the shortened URL.",
+                type: "string",
+              },
+              url: {
+                description: "The shortened URL.",
+                type: "string",
+              },
+              original_url: {
+                description: "The original URL.",
+                type: "string",
+              },
+              status_code: {
+                description: "The status code of the response.",
+                type: "number",
+              },
+            },
+          },
+          "/v1/info": {
+            description: "Returns information about the API.",
+            method: "GET",
+            response: {
+              success: {
+                description: "Whether the request was successful or not.",
+                type: "boolean",
+              },
+              status_code: {
+                description: "The status code of the response.",
+                type: "number",
+              },
+              info: {
+                description: "Information about the API.",
+                type: "object",
+              },
+            },
+          },
+        },
+      },
+      about: {
+        author: "Ishaan Bedi",
+        author_url: "https://www.ishaanbedi.in",
+        github_url: "https://www.github.com/ishaanbedi/api.ishn.xyz",
+        docs_url: "https://api.ishn.xyz",
+        api_url: "https://api.ishn.xyz/v1",
+        support_email: "hi@ishaanbedi.in",
+        twitter_url: "https://www.twitter.com/ishnbedi",
+        note: "This API is offered as-is. Though I will try my best to keep it up and running, I cannot guarantee that it will be up 24/7. If you have any questions, feel free to contact me at hi@ishaanbedi.in or open an issue on GitHub.",
+        issues:
+          "If you find any issues with the API, please open an issue on GitHub.",
+        privacy_policy:
+          "This API does not collect any personal/sensitive data. All data is stored as per the architecture of ishn.xyz using Xata as the database. For more information, please read the privacy policy on ishn.xyz.",
+        license: "This API is licensed under the MIT License.",
+        developer_security_note:
+          "Please do not use this API for any malicious purposes. I will not be held responsible for any misuse of this API. If you find any security vulnerabilities, please let me know ASAP.",
+      },
+    },
+  });
+});
+
+app.get("/v1", async (req, res) => {
+  var { url: queryUrl } = req.query;
+  if (!queryUrl) {
+    res.status(400).json({
+      error: "Missing URL.",
+      reason: "Missing URL as the query parameter.",
+      success: false,
+      status_code: 400,
+      hint: "Please pass the URL as a query parameter. Ex. https://api.ishn.xyz/v1?url=https://www.github.com",
+      more_info: "https://api.ishn.xyz/v1/info",
+    });
+    return;
+  }
+  var parsedUrl = parse(queryUrl);
+  if (!parsedUrl.protocol) {
+    res.status(400).json({
+      error: "Invalid URL.",
+      reason:
+        "The URL you passed is invalid as it does not have a protocol. At present, we support creating URLs for only those websites that have a protocol, such as https:// or http://",
+      success: false,
+      status_code: 400,
+      hint: "Please pass a valid URL as a query parameter. Ex. https://api.ishn.xyz/v1?url=https://www.github.com",
+    });
+    return;
+  }
+
+  var generateRandomSlug = () => {
+    var text = "";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 7; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  };
+  var slug = generateRandomSlug();
+  await xata.db.global_data.create({
+    slug: slug,
+    url: queryUrl,
+  });
+  res.status(201).json({
+    success: true,
+    slug: slug,
+    url: `https://www.ishn.xyz/${slug}`,
+    original_url: queryUrl,
+    status_code: 201,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
