6c118189ca600bb3e9f5ad34a474226b698d0e3e
Ishaan Bedi <bediishaan6@gmail.com>
2022-12-31 23:43:42 +0530

    Reduce Slug Lenght

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ app.get("/v1", async (req, res) => {
     var text = "";
     var possible =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < 7; i++)
+    for (var i = 0; i < 4; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
   };
